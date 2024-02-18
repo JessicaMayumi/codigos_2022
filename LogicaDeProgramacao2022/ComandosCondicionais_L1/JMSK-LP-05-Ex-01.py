5c9571cab01dba54b4e033b4695ec4f31b8d11ce
JessicaMayumi <153833845+JessicaMayumi@users.noreply.github.com>
2024-02-18 13:54:45 -0300

    Create JMSK-LP-05-Ex-01.py

---
+++ LogicaDeProgramacao2022/ComandosCondicionais_L1/JMSK-LP-05-Ex-01.py
@@ -0,0 +1,11 @@
+numero1= int ( input ( " Digite um número: "))
+numero2= int ( input ( " Digite outro número: "))
+
+if numero1 == numero2:
+    print ( " Os números são iguais. ")
+
+elif numero1 > numero2:
+    print ( " O maior número é:", numero1)
+
+else:
+    print ( " O maior número é:", numero2)
