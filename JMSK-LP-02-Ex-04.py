1d0f92ec7c03e43b6891301cb83c4ea77760b3f6
JessicaMayumi <153833845+JessicaMayumi@users.noreply.github.com>
2024-02-18 13:27:55 -0300

    Delete JMSK-LP-02-Ex-04.py

--- JMSK-LP-02-Ex-04.py
+++
@@ -1,4 +0,0 @@
-largura=int(input('Digite a largura do terreno: '))
-profundidade=int(input('Digite a profundidade do terreno: '))
-hectare=(largura*profundidade)/10000
-print('Seu terreno tem a dimensão de {}x{} e sua área é de {} hectares.'.format(largura,profundidade,hectare))
