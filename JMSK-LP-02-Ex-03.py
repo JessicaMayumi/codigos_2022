7850c63fd1e551fd55936e9d4d1ecaa5b42048f0
JessicaMayumi <153833845+JessicaMayumi@users.noreply.github.com>
2024-02-18 13:27:49 -0300

    Delete JMSK-LP-02-Ex-03.py

--- JMSK-LP-02-Ex-03.py
+++
@@ -1,6 +0,0 @@
-
-largura = float(input("Qual é a largura da sua sala?"))
-profundidade= float(input("Qual é a profundidade da sua sala?"))
-
-area=int(largura)*int(profundidade)
-print('Sua sala tem a dimensão de {}x{} e sua área é de {}m².'.format(largura,profundidade,area))
